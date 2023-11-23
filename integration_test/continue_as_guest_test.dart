1225edffa33f71a1fd5d64a4686ee10e5acd867b
Sabir Mondal <sabir.mondal@cloudcraftz.io>
2023-11-23 12:29:30 +0530

    Dev: learning YAML language 12:27 PM

--- integration_test/continue_as_guest_test.dart
+++ integration_test/continue_as_guest_test.dart
@@ -2,7 +2,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:enlight_q_app/app.dart' as app;
 
 void main() {
-  testWidgets('tap on the floating action button, verify counter',
+  testWidgets('tap on ', (tester) async {
+    await tester.pumpWidget(const app.MyApp());
+    tester.pumpAndSettle(const Duration(seconds: 3));
+    expect(1, 1, reason: "This is begining of the set up");
+  });
+  testWidgets('home->continue as guest->fill profile info->logged in',
       (tester) async {
     await tester.pumpWidget(const app.MyApp());
     tester.pumpAndSettle(const Duration(seconds: 3));
