0018fad86d6e598eb6e7e3e312b50dda78b440f5
Sabir Mondal <sabir.mondal@cloudcraftz.io>
2023-11-23 13:16:58 +0530

    Running Test On Windows Latest

--- integration_test/continue_as_guest_test.dart
+++ integration_test/continue_as_guest_test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:enlight_q_app/app.dart' as app;
+import 'package:enlight_q_app/main_prod.dart' as prodApp;
 
 void main() {
+  prodApp.main();
   testWidgets('tap on ', (tester) async {
-    await tester.pumpWidget(const app.MyApp());
     tester.pumpAndSettle(const Duration(seconds: 3));
     expect(1, 1, reason: "This is begining of the set up");
   });
